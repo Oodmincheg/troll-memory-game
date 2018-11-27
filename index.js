03640358f9fd05d74d904c0536d55fe0888388f3
Oodmincheg <Vlad Vorobyov>
2018-11-27 15:54:42 +0200

    change vars names, remove accesing arguments, remove css duplication

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 //array of our cards
 let list = [
   "./img/sad.png",
-  "./img/dad.png",
-  "./img/indiffirent.png",
+  "./img/dad.png"
+  /*"./img/indiffirent.png",
   "./img/pfff.png",
   "./img/troll.png",
   "./img/rage.png",
   "./img/yellow.png",
-  "./img/scream.png"
+  "./img/scream.png"*/
 ];
 //double our cards in the array
 list = list.concat(list);
@@ -17,21 +17,26 @@ const GAME_BOARD = document.getElementById("content");
 const WINNER_STRING = "OK, dude. You are the winner. ";
 const TIME_OF_BACK_FLIP = 1200;
 const TIME_OF_HIDDING = 800;
-let keepPlaying, secondClick, nextMoveFlag, previousFlipper, previousImage;
+let remainingCards,
+  secondClick,
+  performNextMoveFlag,
+  previousFlipper,
+  currentFlipper,
+  previousImage;
 
 //funtcion to flip one or more fliper containers
-const flip = function() {
-  [...arguments].forEach(flipper => {
+const flip = (...args) => {
+  args.forEach(flipper => {
     flipper.classList.toggle("flip");
   });
 };
 
 //function that starts next move and handle game over situation
-const nextMove = () => {
-  if (keepPlaying) {
+const performNextMove = () => {
+  if (remainingCards) {
     secondClick = false;
     previousFlipper = null;
-    nextMoveFlag = true;
+    performNextMoveFlag = true;
   } else {
     GAME_BOARD.textContent = WINNER_STRING;
     let newGameButton = document.createElement("button");
@@ -42,36 +47,36 @@ const nextMove = () => {
 };
 
 // hide matches from page, and count how many cards are left in the game
-function removeMatches(currentFlipper) {
+const removeMatches = () => {
   currentFlipper.classList.add("hidden");
   previousFlipper.classList.add("hidden");
-  keepPlaying -= 2;
-  nextMove();
-}
+  remainingCards -= 2;
+  performNextMove();
+};
 
 //function that handle moves(clicks on the cards)
 const move = event => {
-  const currentFlipper = event.currentTarget;
+  currentFlipper = event.currentTarget;
   // prettier-ignore
-  const currentImage = event.currentTarget.querySelector("img").getAttribute("src");
+  const currentImage = currentFlipper.querySelector("img").getAttribute("src");
 
   //check the click on the same card, click on not hidden element and possibility of opening cards
   if (
     currentFlipper != previousFlipper &&
     !currentFlipper.classList.contains("hidden") &&
-    nextMoveFlag
+    performNextMoveFlag
   ) {
     flip(currentFlipper);
     if (secondClick) {
-      nextMoveFlag = false;
+      performNextMoveFlag = false;
       if (currentImage != previousImage) {
         setTimeout(() => {
           flip(previousFlipper, currentFlipper);
-          nextMove();
+          performNextMove();
         }, TIME_OF_BACK_FLIP);
       } else {
         setTimeout(() => {
-          removeMatches(currentFlipper);
+          removeMatches();
         }, TIME_OF_HIDDING);
       }
     } else {
@@ -84,15 +89,15 @@ const move = event => {
 
 const newGame = () => {
   //shuffle array of cards
-  list.sort(function() {
+  list.sort(() => {
     return 0.5 - Math.random();
   });
 
   //set game's variables to the start values
   GAME_BOARD.textContent = "";
-  keepPlaying = list.length;
+  remainingCards = list.length;
   secondClick = false;
-  nextMoveFlag = true;
+  performNextMoveFlag = true;
 
   //render our cards on the page(back sides)
   list.forEach(img => {
@@ -105,9 +110,7 @@ const newGame = () => {
     flipper.classList.add("flipper");
 
     //add event listnee to our flipper container
-    flipper.addEventListener("mousedown", event => {
-      move(event);
-    });
+    flipper.addEventListener("mousedown", move);
 
     //create image on back side of our card
     let image = document.createElement("img");
@@ -115,8 +118,7 @@ const newGame = () => {
     image.classList.add("back");
 
     //add back and front of our card to the flipper
-    flipper.appendChild(image);
-    flipper.appendChild(blank);
+    flipper.append(image, blank);
 
     //add aour flipper to the page
     GAME_BOARD.appendChild(flipper);
